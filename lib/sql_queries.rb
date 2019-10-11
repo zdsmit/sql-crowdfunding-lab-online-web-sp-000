cd7557855847cbca160d91f48d62f3e0cdde05d1
Zach  Smith <zdsmith001@gmail.com>
2019-10-11 21:31:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT users.name, SUM(pledges.amount) AS pledge_total GROUP BY users.name ORDER BY pledge_total;"
+  "SELECT users.name, SUM(pledges.amount) GROUP BY users.name ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
