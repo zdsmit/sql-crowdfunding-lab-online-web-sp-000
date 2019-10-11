3d24b1cab7849bbcb8e29315327bfb4fa8a76c77
Zach  Smith <zdsmith001@gmail.com>
2019-10-11 21:14:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,11 +11,11 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON pledges.user_id = users.id GROUP BY users.name"
+  "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON pledges.user_id = users.id GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
